d63ff684e186e9ee2acb7d54f0201d912d6bfbd7
Thiago Viccichi <viccichithiago@gmail.com>
2026-03-05 16:21:11 -0300

    Estructura del Proyecto IDEAL

---
+++ src/test/java/com/thiago/imageprocessor/ImageprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.imageprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
