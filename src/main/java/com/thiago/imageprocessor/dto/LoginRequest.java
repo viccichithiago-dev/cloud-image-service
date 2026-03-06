e9a45e1f20698d41c20c2e2f8fba571dbfb60477
Thiago Viccichi <viccichithiago@gmail.com>
2026-03-06 12:30:05 -0300

    Mejoramiento del LoginRequest

--- src/main/java/com/thiago/imageprocessor/dto/LoginRequest.java
+++ src/main/java/com/thiago/imageprocessor/dto/LoginRequest.java
@@ -1,6 +1,10 @@
 package com.thiago.imageprocessor.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class LoginRequest {
     @NotBlank(message="Error el usuario no puede estar vacio")
     @Size(min=3, max=20, message="Error el usuario debe tener entre 3 y 20 caracteres")
