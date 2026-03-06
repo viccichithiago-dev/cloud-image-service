26ef1c5dd3544ddf21ebb194897cc4956d4f271d
Thiago Viccichi <viccichithiago@gmail.com>
2026-03-06 02:52:59 -0300

    Implementancion de DTOS para el JWT de Usuarios, LoginRequest, RegisterRequest y AuthResponse

---
+++ src/main/java/com/thiago/imageprocessor/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.thiago.imageprocessor.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+public class LoginRequest {
+    @NotBlank(message="Error el usuario no puede estar vacio")
+    @Size(min=3, max=20, message="Error el usuario debe tener entre 3 y 20 caracteres")
+    String username;
+    @NotBlank(message="Error la contraseña no puede estar vacia")
+    @Size(min=6, max=20, message="Error la contraseña debe tener entre 6 y 20 caracteres")
+    String password;
+}
