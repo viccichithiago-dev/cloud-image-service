ef1a93c6d811035b3ad259706001acc1599f7ab0
Thiago Viccichi <viccichithiago@gmail.com>
2026-03-13 02:35:02 -0300

    Mejoramiento del modelo mas nuevas implementaciones

--- src/main/java/com/thiago/imageprocessor/model/User.java
+++ src/main/java/com/thiago/imageprocessor/model/User.java
@@ -1,5 +1,11 @@
 package com.thiago.imageprocessor.model;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -19,7 +25,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column()
@@ -50,4 +56,21 @@ public class User {
         this.password = password;
         this.role = Role.USER; // Asignar rol USER por defecto
     }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Esto le dice a Spring qué permisos tiene el usuario
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
